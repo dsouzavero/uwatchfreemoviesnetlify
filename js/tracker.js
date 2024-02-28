abac204beaa8771b02be1daa6901e2f372655d2b
veigasjeff <154071076+dsouzavero@users.noreply.github.com>
2024-02-28 17:39:02 +0530

    update tracker

---
+++ js/tracker.js
@@ -0,0 +1,16 @@
+// googlescript.js
+
+(function () {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-MW02Y2K0Z2';
+  
+    document.head.appendChild(script);
+  
+    script.onload = function () {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() { dataLayer.push(arguments); }
+      gtag('js', new Date());
+      gtag('config', 'G-MW02Y2K0Z2');
+    };
+  })();
